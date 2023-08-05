ecc8f802b6ef3ab742c0944d320beb3834b12e8c
wilfredcloud <wilfredcloudspace@gmail.com>
2023-08-05 13:23:28 +0100

    feat: Add cache_id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -6,6 +6,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
+import uuid
 from flask import Flask, render_template
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -33,11 +34,14 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
+    cache_id = str(uuid.uuid4())
 
-    return render_template('0-hbnb.html',
+    return render_template('0-hbnb.html',   
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id
+                           )
 
 
 if __name__ == "__main__":
